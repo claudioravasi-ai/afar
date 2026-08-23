87f6a58de5a632fbfc909eb6eaa079463d63a0d4
claudioravasi-ai <claudio.ravasi@gmail.com>
2026-08-23 15:00:02 -0300

    Add files via upload

--- build.py
+++ build.py
@@ -101,6 +101,7 @@ def main():
 <body>
 %(body)s
 <script src="https://www.gstatic.com/firebasejs/10.14.1/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.14.1/firebase-auth-compat.js"></script>
 <script src="https://www.gstatic.com/firebasejs/10.14.1/firebase-database-compat.js"></script>
 <script>
 %(js)s
